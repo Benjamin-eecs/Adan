67502c77c33a4639f4654cc254ad8cd421491a2e
Benjamin-eecs <benjaminliu.eecs@gmail.com>
2023-07-25 22:28:30 +0800

    fix: update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ import os
 import sys
 from setuptools import setup
 
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-from torch.cuda import is_available
+# from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+# from torch.cuda import is_available
 
 if "--unfused" in sys.argv:
     print("Building unfused version of adan")
     sys.argv.remove("--unfused")
-else:
-    cuda_extension = CUDAExtension(
-            'fused_adan',
-            sources=['fused_adan/pybind_adan.cpp','./fused_adan/fused_adan_kernel.cu', './fused_adan/multi_tensor_adan_kernel.cu']
-        )
+# else:
+#     cuda_extension = CUDAExtension(
+#             'fused_adan',
+#             sources=['fused_adan/pybind_adan.cpp','./fused_adan/fused_adan_kernel.cu', './fused_adan/multi_tensor_adan_kernel.cu']
+#         )
 
-build_cuda_ext = is_available() or os.getenv('FORCE_CUDA', '0') == '1'
+# build_cuda_ext = is_available() or os.getenv('FORCE_CUDA', '0') == '1'
 
 setup(
     name='adan',
@@ -31,5 +31,5 @@ setup(
         'Lin, Zhouchen and Yan, Shuicheng'
     ),
     ext_modules=[],
-    cmdclass={'build_ext': BuildExtension} if build_cuda_ext else {},
+    cmdclass={},
 )
